4e09631ac8fef37a7f8e5d01b802540f3730bda9
jonathan <achola.jon@yahoo.com>
2022-04-07 04:57:38 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - entry point
+ * Return: return (1)
+ */
+int main(four);
+four()
+{
+printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (0);
+}
