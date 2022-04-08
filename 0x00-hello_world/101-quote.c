3bd732661a2e6a8c51fcba0dec5ab494046af68d
jonathan <achola.jon@yahoo.com>
2022-04-08 15:51:09 -0700

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,9 +3,8 @@
  * main - entry point
  * Return: return (1)
  */
-int main(four);
-four()
+int main(void)
 {
-printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59;
+	return (1);
 }
