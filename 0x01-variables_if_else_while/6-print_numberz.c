4888889529564c5f2b9db8b3618f281c70178d84
jonathan <achola.jon@yahoo.com>
2022-04-08 09:26:15 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 0 when success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
