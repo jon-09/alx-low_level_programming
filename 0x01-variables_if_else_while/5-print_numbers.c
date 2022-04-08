6c87ad86ad94cafa9d424b5374a4c04d7f2562a6
jonathan <achola.jon@yahoo.com>
2022-04-08 09:18:41 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - -entry point
+ * Return: returnd 0 if success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%x", x);
+	}
+	printf("\n");
+	return (0);
+}
