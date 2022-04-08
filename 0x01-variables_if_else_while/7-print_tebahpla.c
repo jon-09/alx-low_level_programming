fa16c46d20b985d7455f78bfb13d6233726751f2
jonathan <achola.jon@yahoo.com>
2022-04-08 10:04:56 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: return 0 when successfull
+ */
+int main(void)
+{
+	char x;
+
+	for ( x = 122; x >= 97; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
