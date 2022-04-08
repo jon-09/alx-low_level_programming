1a8b4655d0f977baeacb8ce9b4be667bf0662297
jonathan <achola.jon@yahoo.com>
2022-04-08 08:41:22 -0700

    2. I sometimes suffer from insomnia, so iplay the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: return (0) whne successful
+ */
+int main()
+{
+	char q;
+
+	for (q = 'a'; q <= 'z'; q++)
+		putchar(q);
+	
+	putchar('\n');
+	return (0);
+}
