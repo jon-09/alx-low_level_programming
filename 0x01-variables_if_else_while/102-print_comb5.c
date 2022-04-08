4591b2bc3dcfe859f0a302c4283346c72adc2b6d
jonathan <achola.jon@yahoo.com>
2022-04-08 14:04:53 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main -entyr point
+ * Return: returns 0 when successful
+ */
+int main(void)
+{
+	int i, x, y, z;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (x = 48; x <= 57; x++)
+		{
+			for (y = 48; y <= 57; y++)
+			{
+				for (z = 48; z <= 57; z++)
+				{
+					putchar(i);
+					putchar(x);
+					putchar(' ');
+					putchar(y);
+					putchar(z);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
