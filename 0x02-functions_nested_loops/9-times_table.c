c42b2c3833a8f0fb6ef507fb37f1f3f54d9174a0
jonathan <achola.jon@yahoo.com>
2022-04-12 04:43:52 -0700

    betty style task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 				if (b != 0)
 				{
 					_putchar(' ');
-					_putchar((c %10) + '0');
+					_putchar((c % 10) + '0');
 				}
 				if (b < 9)
 				{
@@ -39,7 +39,7 @@ void times_table(void)
 				if (b < 9)
 				{
 					_putchar(',');
-					_putchar(' ' );
+					_putchar(' ');
 				}
 			}
 		}
