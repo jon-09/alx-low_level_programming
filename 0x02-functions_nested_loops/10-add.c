8ac5d2a61240169e4dae568291a1597938659cf2
jonathan <achola.jon@yahoo.com>
2022-04-12 06:07:00 -0700

    task 10 betty correction

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
- 
+
 /**
   * add - prints the addition of two numbers
   * @i: print int i
-  * @k print int k
+  * @k: print int k
   * Return: always 0
   */
 int add(int i, int k)
