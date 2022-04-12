42db6ccbe94f714cc1310d21badfefbac3a6e0a9
jonathan <achola.jon@yahoo.com>
2022-04-12 06:20:01 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,9 @@
-include "main.h"
+#include "main.h"
 #include <stdio.h>
 
 /**
  * print_to_98 -prints all natural numbers fro n to 98
+ * user input's number prints to 98, regardless < 98 or > 98
  * @n: number to start count
  * Return: alwyas returns 0
  */
@@ -10,18 +11,24 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n =n ; n < 98; n++)
+		while (n <= 98)
 		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
+			printf("%d", n);
+			if (n != 98)
+				printf(",");
+			n++;
+		}
+	}
+	else if (n >= 98)
+	{
+		while (n >= 98)
+		{
+			if (n != 98)
+				printf(",");
+			n--;
 		}
 	}
 	else
-	{
-		for (n = n; n > 98; n--)
-		{
-			printf("%d, ", n);
-			printf("%d\n", 98);
-		}
-	}
+		printf("98");
+	printf("\n");
 }
