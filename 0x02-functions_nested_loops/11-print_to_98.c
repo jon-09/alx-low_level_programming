7da166bac19a0f94cd2e877b0674f9a2e166d9a7
jonathan <achola.jon@yahoo.com>
2022-04-12 06:30:39 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,20 +15,27 @@ void print_to_98(int n)
 		{
 			printf("%d", n);
 			if (n != 98)
+			{
 				printf(", ");
+			}
 			n++;
 		}
 	}
-	else if (n >= 98)
+	else if (n > 98)
 	{
 		while (n >= 98)
 		{
+			printf("%d", n);
 			if (n != 98)
+			{
 				printf(", ");
+			}
 			n--;
 		}
 	}
 	else
+	{
 		printf("98");
-	printf("\n");;
+	}
+	printf("\n");
 }
