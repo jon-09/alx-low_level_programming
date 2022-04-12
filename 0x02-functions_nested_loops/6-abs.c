3b8d9245c96400fb9da26a7b44f54fac696bfe70
jonathan <achola.jon@yahoo.com>
2022-04-12 02:12:50 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - value absolute
+ * Return: always 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
