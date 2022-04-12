5978724af717ea2ec601688f55a0c91dcec4e721
jonathan <achola.jon@yahoo.com>
2022-04-12 07:08:10 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,12 +8,17 @@
 void print_times_table(int n)
 {
 	int i;
+	int x;
+
 	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (x = 0; x <= n; x++)
 		{
-			int multiple = (i * n);
-			printf("%d, ", multiple);
+			for (i = 0; i <= n; i++)
+			{
+				int multiple = (i * n);
+				printf("%d, ", multiple);
+			}
 		}
 	}
 	else
