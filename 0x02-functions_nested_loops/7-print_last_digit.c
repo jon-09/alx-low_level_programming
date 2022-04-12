db5cf542657728675997abdffe5e819818e897e4
jonathan <achola.jon@yahoo.com>
2022-04-12 02:17:52 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+ /**
+  * print_last_digit - last digit
+  * Return: always 0
+  */
+int print_last_digit(int n)
+{
+	int t;
+
+	if (n < 0)
+	{
+		t = (-1 * (n % 10));
+		_putchar(t + '0');
+		return (t);
+	}
+	else
+	{
+		t =(n % 10);
+		_putchar(t + '0');
+		return(t)
+	}
+}
