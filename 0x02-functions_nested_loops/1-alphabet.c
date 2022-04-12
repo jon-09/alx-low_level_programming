81b7ed30731b9749c50a7f94e074a3d3d1189859
jonathan <achola.jon@yahoo.com>
2022-04-12 01:49:50 -0700

    tastk 2 and changes to task 1 and main.h

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,8 +8,9 @@ void print_alphabet(void)
 	char x;
 
 	for (x = 'a'; x <= 'z'; x++)
+	{
 		_putchar(x);
+	}
 	_putchar('\n');
 
-	return 0;
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphBET_X10 - ENTRY
+ * Returns: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int alp;
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+		for (alp = 'a'; alp <= 'z'; alp++)
+		{
+			_putchar(alp);
+		}
+		count++;
+	}
+}
