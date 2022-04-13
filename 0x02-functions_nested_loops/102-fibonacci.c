98a2afd6a93d8228af4c9a58bb07416ca2505709
jonathan <achola.jon@yahoo.com>
2022-04-13 12:14:16 -0700

    fibs

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints the first 50 fibs number, with 1 and 2
+ * followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	long int i, j, k, next;
+	
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 2036501174)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	return (0);
+}
