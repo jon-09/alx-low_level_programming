3c57d1b612c2a46584a9e1b586bf37f4c3f17d70
jonathan <achola.jon@yahoo.com>
2022-04-15 03:10:42 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,10 +11,10 @@ void more_numbers(void)
 	{
 		for (x = 0; x <= 14; x++)
 		{
-			if (x >= 10)
-				_putchar((x / 10) + '0');
-			_puchar((x % 10) +'0');
+			_puchar(x);
 		}
 		_putchar('\n');
 	}
+	_putchar('\n');
+	return (0);
 }
