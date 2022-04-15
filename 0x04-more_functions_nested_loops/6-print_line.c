149c8b4fc75e1925d9801090a866fef31967cfa0
jonathan <achola.jon@yahoo.com>
2022-04-15 01:48:47 -0700

    task 6 corrections

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_lin - function draws a straignt line in the terminal
- * @n: nimber of times '_' to be printed
+ * print_line - function draws a straignt line in the terminal
+ * @n: number of times '_' to be printed
  * Return: Always 0 when successful
  */
 void print_line(int n)
