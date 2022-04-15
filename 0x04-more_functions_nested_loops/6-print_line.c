55740e75198daf99971d716901b631fab72dc191
jonathan <achola.jon@yahoo.com>
2022-04-15 01:44:55 -0700

    task 6 corrections

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,10 +9,12 @@ void print_line(int n)
 	int line;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
-		for (line = 1; line <=n; line++)
+		for (line = 1; line <= n; line++)
 			_putchar('_');
 	}
 	_putchar('\n');
