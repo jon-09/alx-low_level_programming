ca54aebf712db6a5b0c625a779d274b6316c6e45
jonathan <achola.jon@yahoo.com>
2022-04-19 00:31:22 -0700

    task 12 corrections

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,10 +1,31 @@
 #include "main.h"
 /**
  * print_number - function prints the number passed
- * @n: the number to be printed
+ * @n: input integer
  * Return: Always 0 when successful
  */
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int x, y, z;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		y = n * -1;
+	}
+	else
+	{
+		y = n;
+	}
+	x = y;
+	z = 1;
+	while (x > 9)
+	{
+		x /= 10;
+		z *= 10;
+	}
+	for (; z >= 1; z /= 10)
+	{
+		_putchar((( y / z) % 10) + 48);
+	}
 }
