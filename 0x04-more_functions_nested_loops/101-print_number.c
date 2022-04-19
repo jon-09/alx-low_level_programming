ff564367431158bb64d0f94fa720230d617e2950
jonathan <achola.jon@yahoo.com>
2022-04-18 23:33:22 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_number - function prints the number passed
+ * @n: the number to be printed
+ * Return: Always 0 when successful
+ */
+void print_number(int n)
+{
+	_putchar(n);
+}
