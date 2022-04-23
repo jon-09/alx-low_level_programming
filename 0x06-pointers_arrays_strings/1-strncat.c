d66791200b3c3182401a49c4b44c1dc9344ca364
jonathan <achola.jon@yahoo.com>
2022-04-23 02:05:04 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * *_strncat - function concatenates string with specifi size
+ * @src - source
+ * @dest - destination
+ * @n - size of the bytes tobe concatenated from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	y = 0;
+	
+	/* find the NULL byte of the dest */
+	while (*(dest + x) != '\0')
+	{
+		x++;
+	}
+	while (y <= n)
+	{
+		*(dest + x) = *(src + y);
+		if (*(src + y) == '\0')
+			break;
+		x++;
+		y++;
+	}
+	return (dest);
+}
