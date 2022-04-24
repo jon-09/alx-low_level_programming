ea7a331fa69757be12bce0c2ec592fcb93f540e3
jonathan <achola.jon@yahoo.com>
2022-04-24 00:43:44 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,15 +8,17 @@
 void reverse_array(int *a, int n)
 {
 	int *strt, *revr, x;
-	int i;
+	int i, y, z;
 
 	strt = a;
 	revr = a;
-	for (i = 0; i < n-1; i++)
+	y = n - 1;
+	z = n / 2;
+	for (i = 0; i < y; i++)
 	{
 		revr++;
 	}
-	for (i = 0; i < n / 2; i++)
+	for (i = 0; i < z; i++)
 	{
 		x = *revr;
 		*revr = *strt;
