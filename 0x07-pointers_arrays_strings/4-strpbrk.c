a463bb866d8d44653ecbf3e1eef7db2269fc7895
jonathan <achola.jon@yahoo.com>
2022-04-26 09:55:57 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strpbrk - entry point
+ * @s: input
+ * @accept: input
+ * Return: 0 when successful
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int ocr = 0;
+
+	for (; accept[ocr] != '\0'; ocr++)
+	{
+		if (accept[ocr] == s[ocr])
+			return (*s[ocr]);
+		else
+			return (0);
+	}
+}
