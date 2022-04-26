edbb067457c2d7da4594bb00564d0858e95ee8f8
jonathan <achola.jon@yahoo.com>
2022-04-26 06:07:47 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _memset() - function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ * @s - the destination
+ * @b - character to be used in the replacement process
+ * @n - the number of bytes to be replaced by b from s
+ * Return - returns the pointer when successful
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(*(s + n), 'b', n);
+
+	return (s);
+}
