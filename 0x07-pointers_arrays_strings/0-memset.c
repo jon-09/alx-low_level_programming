803a71b5e81c1ab09da1593455c607ee17475ca1
jonathan <achola.jon@yahoo.com>
2022-04-26 07:18:34 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,11 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
-
-	for (; i < n; i++)
+	int i;
+	
+	while ((n > 0) && (i => 0))
 	{
 		s[i] = b;
+		i++;
+		n--;
 	}
 	return (s);
 }
