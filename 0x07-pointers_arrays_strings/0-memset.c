f62d2abe05a4b994bbeb7641af8f7494b277932e
jonathan <achola.jon@yahoo.com>
2022-04-26 07:47:55 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
  * _memset - function fills a memory set with specific value
- * @s - address of the memory to be filled
- * @b - character to be used in the replacement process
- * @n - the number of bytes to be replaced
- * Return - returns the pointer when successful
+ * @s: address of the memory to be filled
+ * @b: character to be used in the replacement process
+ * @n: the number of bytes to be replaced
+ * Return: returns the value of array after the change
  */
 char *_memset(char *s, char b, unsigned int n)
 {
