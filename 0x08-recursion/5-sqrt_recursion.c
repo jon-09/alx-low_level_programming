64e663041098cba1e80dcaf52c359f2e70d09586
jonathan <achola.jon@yahoo.com>
2022-04-28 03:14:02 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -21,7 +21,7 @@ int _recursion(int x, int y)
 		return (_recursion(x, y + 1));
 }
 
-// _sqrt_recursion - function returns natura square root of a number
+/* _sqrt_recursion - function returns natural a number */
 int _sqrt_recursion(int n)
 {
 	if (n <= 0)
