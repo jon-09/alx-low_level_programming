42e7a3ffc87df809bca7cfd3a0d275d985d8fb2c
jonathan <achola.jon@yahoo.com>
2022-04-28 01:37:34 -0700

    task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - reverses the string given
+ * @s: pointer to the string to be reversed
+ * Return: Always 0 when successful
+ */
+void _print_rev_recursion(char *s)
+{
+	if ( *s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_print_rev_recursion(s + a);
+		_putchar(*s);
+	}
+}
