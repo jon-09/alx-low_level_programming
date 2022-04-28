a0bf28c41279103601377e988f4615e315342e23
jonathan <achola.jon@yahoo.com>
2022-04-28 03:58:30 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,13 +6,13 @@
  */
 int is_prime_number(int n)
 {
-	if (prime <= 1 )
+	if (n <= 1 )
 		return (0);
-	else if (prime == 2)
+	else if (n == 2)
 		return (1);
-	else if (prime % 2 == 0 || prime % 3 == 0 || prime % 5 == 0)
+	else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0)
 		return (0);
-	else if (prime % 7 == 0 || prime % 9 == 0)
+	else if (n % 7 == 0 || n % 9 == 0)
 		return (0);
 	else 
 		return (1);
