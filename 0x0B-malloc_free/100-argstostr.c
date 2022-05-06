89a8387f28b3e68319b47fb63012fda73f853aeb
jonathan <achola.jon@yahoo.com>
2022-05-05 22:13:00 -0700

    tast 5

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * argstostr - function concatenates all the args within the program
+ * @ac: corresponds to the width of the grid
+ * @av: corresponds to the height of the grid
+ * Return: returns the args in grid
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int size, x, y, z;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	x = 0;
+	y = 0;
+	z = 0;
+	while (x < ac)
+	{
+		while (av[x][y] != '\0')
+		{
+			size++;
+			y++;
+		}
+		x++;
+	}
+	size = size + ac + 1;
+	str = malloc(sizeof(char) * size);
+	if (str == NULL)
+		return (NULL);
+
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y] != '\0'; y++)
+		{
+			str[z] = av[x][y];
+			z++;
+		}
+		str[z] = '\n';
+		z++;
+	}
+	return (str);
+}
