0a937743896d225da21994fe6c27570adf2e63f7
jonathan <achola.jon@yahoo.com>
2022-05-07 01:31:49 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * main- entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int n, m, ad;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (n = 1; n < argc; n++)
+	{
+		for (m =0; argv[n][m] != '\0'; m++)
+		{
+			if (!(isdigit(argv[n][m])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		ad += atoi(argv[n]);
+	}
+	printf("%d\n", ad);
+
+	return (0);
+}
