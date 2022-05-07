9549ac3a305b51fd438105d8bea73cd6c454cb83
jonathan <achola.jon@yahoo.com>
2022-05-07 01:00:32 -0700

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+			mul *= argv[i];
+
+		printf("%d\n", mul);
+
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
