d4bd52f9736d230bbb47ae7ddb8f251f8070b9e6
jonathan <achola.jon@yahoo.com>
2022-05-06 23:38:23 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argv: the argument
+ * Return: Always 0
+ */
+int main(char *argv[])
+{
+	print("%s\n", argv[0]);
+
+	return (0);
+}
