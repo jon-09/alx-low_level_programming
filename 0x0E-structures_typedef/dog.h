d81749ca60bd6ae2c69fb3b6006016066c9c0ad3
jonathan <achola.jon@yahoo.com>
2022-05-12 04:20:35 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure named dog
+ * @name: character input
+ * @age: float input
+ * @owner: character input
+ * Discription: struct named dog
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
