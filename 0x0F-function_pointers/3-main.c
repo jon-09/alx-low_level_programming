e0ca57720aff307c61ba1753a74ee02f5f9679a9
jonathan <achola.jon@yahoo.com>
2022-05-16 06:35:38 -0700

    main.c task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+/**
+ * main - entry point
+ * @argc: number of arguments passed
+ * @argv: array of arguements
+ * Return: Always 0
+ */
+int main(int argc, char *argv)
+{
+	int n, m;
+	char opr;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	n = atoi(argv[1]);
+	m = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	if (!func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	opr = *argv[2];
+	if (opr == '/' || opr == '%' && m == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", func(n, m));
+	return (0);
+}
