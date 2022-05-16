5cedf237d982b13e97d992312b109e4c0aa2cf22
jonathan <achola.jon@yahoo.com>
2022-05-16 04:51:52 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name- function prints the name passed
+ * print_name - function prints the name passed
  * @name: character input
  * @f: point to function f
  * Return: void
