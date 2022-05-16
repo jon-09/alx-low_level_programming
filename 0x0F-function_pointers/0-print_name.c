6e38768d1c18f039db69f4ca225947057498f6a6
jonathan <achola.jon@yahoo.com>
2022-05-16 04:48:56 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name- function prints the name passed
+ * @name: character input
+ * @f: point to function f
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+			return;
+
+	f(name);
+}
