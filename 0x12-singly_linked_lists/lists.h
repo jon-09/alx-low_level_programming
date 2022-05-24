fec79f03f3a5db1ea3719058771b78fe3e59602c
jon <achola.jon@yahoo>
2022-05-24 10:38:28 -0700

    task 4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 
 typedef struct list_t
 {
@@ -16,5 +17,6 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
