4aee27409574ac1ecf037507f11680fb35aa5854
jon <achola.jon@yahoo>
2022-05-24 08:26:13 -0700

    changes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - function prints all the elements
+ * @h: linked list
+ * Return: number of elements in the list
+ */
+size_t print_list(const list_t *h)
+{
+	size_t itm;
+
+	for (itm = 0; h != NULL; itm++)
+	{
+		if ((*h).str != NULL)
+			printf("[0] %s", "(nill)");
+
+		else
+			printf("[%d] %s", (*h).len, (*h).str);
+
+		h = (*h).next;
+	}
+	return (itm);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct list_t
+{
+	char *str;
+	unsigned int len;
+	struct list_t *next;
+} list_t;
+
+size_t print_list(const list_t *h);
+
+
+#endif
