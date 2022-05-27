4d04c6d6ba433ca187d998d35493b8b70b74f763
jon <achola.jon@yahoo>
2022-05-27 09:08:47 -0700

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -6,9 +6,12 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
+	int count = 0;
 	while (head != NULL)
 	{
 		printf("%d\n", head->n);
+		count += 1;
 		head = head->next;
 	}
+	return (count);
 }
