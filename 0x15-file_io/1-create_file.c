64089e45bf76631d17f0f79194c2aafeec600586
jon <achola.jon@yahoo.com>
2022-06-02 08:38:13 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,10 +13,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		text_content = "";
+		text_content = " ";
 
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	if (file == -1)
 		return (-1);
 
@@ -25,7 +24,6 @@ int create_file(const char *filename, char *text_content)
 		i++;
 
 	filWrite = write(file, text_content, i);
-
 	if (filWrite == -1)
 		return (-1);
 
