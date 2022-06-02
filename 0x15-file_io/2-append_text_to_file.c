d5c8dd6773128cd1fe8a74d2daa21b7b43e0f9ac
jon <achola.jon@yahoo.com>
2022-06-02 09:00:13 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_APPEND);
+	file = open(filename, O_APPEND | O_RDWR, 0664);
 
 	if (file == -1)
 		return (-1);
