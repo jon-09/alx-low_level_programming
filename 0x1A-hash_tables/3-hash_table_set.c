9ea7e2f69aca91ae26db8df67104f9a2e8bf4eca
Jonathan <achola.jon@yahoo.com>
2022-07-30 10:39:16 +0300

    an existing hash-table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,63 +1,52 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_set - set a node in array
- * @ht: hash table you want to add or update the key/value to
- * @key: key can not be an empty string
- * @value: value associated with the key
+ * hash_table_set - adds an element to a hash table.
+ * @ht: hash table set.
+ * @key: key of the hash node to add.
+ * @value: value associated with the key.
  *
- * Return: hash_table or NULL
-*/
+ * Return: 1 if suceeds, 0 otherwise.
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int new;
-	hash_node_t *temp = NULL;
+	unsigned long int position = 0;
+	hash_node_t *new_hash_node, *hash_node_list;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
-	if (key != NULL || value != NULL)
+	position = key_index((const unsigned char *)key, ht->size);
+	new_hash_node = malloc(sizeof(hash_node_t));
+	new_hash_node->key = (char *)strdup(key);
+	new_hash_node->value = (char *)strdup(value);
+	if (new_hash_node == NULL || new_hash_node->key == NULL ||
+		new_hash_node->value == NULL)
 	{
-		new = key_index((unsigned char *)key, ht->size);
-		temp = malloc(sizeof(hash_node_t));
-		if (temp == NULL)
-			return (0);
-		temp->key = strdup(key);
-		temp->value = strdup(value);
-		temp->next = NULL;
-		if (check(ht, key))
-		{
-			(ht->array[new])->value = temp->value;
-			return (1);
-		}
-		if (ht->array[new] == NULL)
-			ht->array[new] = temp;
-		else
-		{
-			temp->next = ht->array[new];
-			ht->array[new] = temp;
-		}
+		free(new_hash_node);
+		free(new_hash_node->key);
+		free(new_hash_node->value);
+		return (0);
+	}
+	if (ht->array[position] == NULL)
+	{
+		ht->array[position] = new_hash_node;
+		new_hash_node->next = NULL;
 		return (1);
 	}
-	return (0);
-}
-/**
- * check - check if the current linked ist is being overwirten
- * @ht: hash table
- * @key: key to be checked
- *
- * Return: 1 if same 0 if not
- */
-int check(hash_table_t *ht, const char *key)
-{
-	hash_node_t *temp;
-	unsigned long int new;
-
-	new = key_index((unsigned char *)key, ht->size);
-	temp = ht->array[new];
-	while (temp != NULL)
+	hash_node_list = ht->array[position];
+	while (hash_node_list != NULL)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(hash_node_list->key, key) == 0)
+		{
+			free(new_hash_node->value);
+			free(new_hash_node->key);
+			free(new_hash_node);
+			ht->array[position]->value = (char *)value;
 			return (1);
-		temp = temp->next;
+		}
+		hash_node_list = hash_node_list->next;
 	}
-	return (0);
+	new_hash_node->next = ht->array[position];
+	ht->array[position] = new_hash_node;
+	return (1);
 }
